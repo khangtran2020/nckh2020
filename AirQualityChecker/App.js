cb4d689322073f705d84ad83e18a5afc12445566
khangtran <kt36@njit.edu>
2020-10-11 14:12:14 -0400

    Change code to slidebar

--- AirQualityChecker/App.js
+++ AirQualityChecker/App.js
@@ -1,8 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
+//React Native Slider
+//https://aboutreact.com/react-native-slider/
+
+//import React in our code
+import React, {useState} from 'react';
+
+//import all the components we are going to use
+import {View, Text, SafeAreaView, StyleSheet, Button, Alert} from 'react-native';
+import Slider from '@react-native-community/slider';
 import * as firebase from "firebase";
-import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBhL-mZYwOU3w7cm6MArYbtQwf8UJK9SZg",
@@ -14,79 +19,123 @@ const firebaseConfig = {
   appId: "1:482032220258:web:1b45e0601fcf495d96793b",
   measurementId: "G-XQKPXGCR70",
 };
-
 if (firebase.apps.length == 0) {
   firebase.initializeApp(firebaseConfig);
 }
 
 var temperature = 0;
+var power = 0;
 var state = "";
 console.disableYellowBox = true;
 const db = firebase.database();
 
-export default function App() {
+// const [sliderValue, setSliderValue] = useState(15);
+
+
+const App = () => {
+  const [sliderValue, setSliderValue] = useState(15);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>This is the Air Quality Checker app for NCKH 2020</Text>
-      <Button
-        title="Get Data"
-        onPress={() => {
-          firebase
-            .database()
-            .ref("M2Q")
-            .on("value", (snapshot) => {
-              // console.log(snapshot);
-              temperature = snapshot.val().Value;
-              console.log("Temperature:" + temperature);
-              Alert.alert("Temperature:" + temperature);
-            });
-        }}
-      />
-      <StatusBar style="auto" />
-      <Button
-        title="Turn On"
-        onPress={
-          () => {
-            firebase.database().ref("Vaccum").once("value").then(function(snapshot) {
-              state = snapshot.val().value;
-              if (state == "off") {
-                firebase.database().ref("Vaccum").update({"value": "on"}).then(() => Alert.alert("Turned on"));
-              }
-              else {
-                console.log("State of Vaccum: " + state);
-                Alert.alert("The vaccum machine is already on");
-              }
-            })
-          }
-        }
-        />
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        {/*Text to show slider value*/}
+        <Text>Hello World!</Text>
+        <Text>This is the Air Quality Checker app for NCKH 2020</Text>
+        <Text>Click the button bellow to get the data</Text>
         <Button
-          title="Turn Off"
+          title="Get Data"
+          onPress={() => {
+            firebase
+              .database()
+              .ref("M2Q")
+              .on("value", (snapshot) => {
+                // console.log(snapshot);
+                temperature = snapshot.val().Value;
+                console.log("Temperature:" + temperature);
+                Alert.alert("Temperature:" + temperature);
+              });
+          }}
+        />
+        <Text>Click the button bellow to turn on the vaccum</Text>
+        <Button
+          title="Turn On"
           onPress={
             () => {
-              firebase.database().ref("Vaccum").once("value").then(function(snapshot) {
-                state = snapshot.val().value;
+              firebase.database().ref("Vacum").once("value").then(function(snapshot) {
+                state = snapshot.val().State;
+                if (state == "off") {
+                  firebase.database().ref("Vacum").update({"State": "on"}).then(() => Alert.alert("Turned on"));
+                }
+                else {
+                  console.log("State of Vaccum: " + state);
+                  Alert.alert("The vaccum machine is already on");
+                }
+              })
+            }
+          }
+          />
+          <Text>Click the button bellow to turn off the vaccum</Text>
+          <Button
+            title="Turn Off"
+            onPress={
+              () => {
+                firebase.database().ref("Vacum").once("value").then(function(snapshot) {
+                  state = snapshot.val().State;
+                  if (state == "on") {
+                    firebase.database().ref("Vacum").update({"State": "off"}).then(() => Alert.alert("Turned off"));
+                    firebase.database().ref("Vacum").update({"Power": 0});
+                    setSliderValue(0);
+                  }
+                  else {
+                    console.log("State of Vaccum: " + state);
+                    Alert.alert("The vaccum machine is already off");
+                  }
+                });
+              }
+            }
+            />
+        <Text style={{color: 'black'}}>
+           Power of Vaccum : {sliderValue}
+        </Text>
+
+
+        {/*Slider with max, min, step and initial value*/}
+        <Slider
+          maximumValue={100}
+          minimumValue={0}
+          minimumTrackTintColor="#307ecc"
+          maximumTrackTintColor="#000000"
+          step={1}
+          value={sliderValue}
+          onValueChange={
+            (sliderValue) => {
+              firebase.database().ref("Vacum").once("value").then(function(snapshot) {
+                state = snapshot.val().State;
                 if (state == "on") {
-                  firebase.database().ref("Vaccum").update({"value": "off"}).then(() => Alert.alert("Turned off"));
+                  setSliderValue(sliderValue);
+                  firebase.database().ref("Vacum").update({"Power": sliderValue});
                 }
                 else {
                   console.log("State of Vaccum: " + state);
                   Alert.alert("The vaccum machine is already off");
+                  setSliderValue(0);
                 }
               });
             }
           }
-          />
-    </View>
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
 });
+
+export default App;
